d313574d4adc523b69480c9744d8c6702ef72943
Ondrej Sedlacek <xsedla1o@stud.fit.vutbr.cz>
2026-05-19 12:44:23 +0200

    Fix Charlie's menu parsing

--- modules/charlies.php
+++ modules/charlies.php
@@ -18,39 +18,68 @@ class Charlies extends LunchMenuSource
 		$today = get_czech_day(date('w', $todayDate));
 
 		$content = $cached['html']->find("div.entry-content", 0);
-		if (!$content) {
-			throw new ScrapingFailedException("div.entry-content not found");
-		}
+		$tables = $content? $content->find("table.menu-one-day") : array();
 
-		$tables = $content->find("table.menu-one-day");
-		if (!$tables) {
-			throw new ScrapingFailedException("table.menu-one-day not found");
-		}
-
-		foreach ($tables as $table) {
-			$title = $this->normalizeText($table->find("th.table-title", 0)->plaintext);
-			$title_lc = mb_strtolower($title);
-
-			if (strpos($title_lc, $today) !== FALSE) {
-				// todays menu
-				$this->processTable($result, null, $table);
-
-			} else {
-				$isOtherDay = false;
-				foreach(get_all_czech_days() as $day) {
-					if (strpos($title_lc, $day) !== FALSE) {
-						$isOtherDay = true;
-						break;
-					}
-				}
-
-				if (!$isOtherDay) {
-					$this->processTable($result, $title, $table);
-				}
+		if ($tables) {
+			foreach ($tables as $table) {
+				$title = $this->normalizeText($table->find("th.table-title", 0)->plaintext);
+				$this->processMenuBlock($result, $today, $title, $table, true);
 			}
+
+			return $result;
+		}
+
+		$menus = $cached['html']->find("div.menu-one-day");
+		if (!$menus) {
+			throw new ScrapingFailedException("menu-one-day not found");
+		}
+
+		foreach ($menus as $menu) {
+			$title = '';
+			$parent = $menu->parent();
+			$titleElement = $parent? $parent->find("h2", 0) : null;
+			if ($titleElement) {
+				$title = $this->normalizeText($titleElement->plaintext);
+			}
+			$this->processMenuBlock($result, $today, $title, $menu, false);
 		}
 
 		return $result;
+	}
+
+	protected function processMenuBlock(&$result, $today, $title, $block, $isTable)
+	{
+		$title_lc = mb_strtolower($title);
+		if ($title_lc == 'nabídka baru') {
+			return;
+		}
+
+		if (strpos($title_lc, $today) !== FALSE) {
+			// todays menu
+			$this->processDishes($result, null, $block, $isTable);
+
+		} else {
+			$isOtherDay = false;
+			foreach(get_all_czech_days() as $day) {
+				if (strpos($title_lc, $day) !== FALSE) {
+					$isOtherDay = true;
+					break;
+				}
+			}
+
+			if (!$isOtherDay) {
+				$this->processDishes($result, $title, $block, $isTable);
+			}
+		}
+	}
+
+	protected function processDishes(&$result, $group, $block, $isTable)
+	{
+		if ($isTable) {
+			$this->processTable($result, $group, $block);
+		} else {
+			$this->processDivMenu($result, $group, $block);
+		}
 	}
 
 	protected function processTable(&$result, $group, $table)
@@ -67,8 +96,43 @@ class Charlies extends LunchMenuSource
 		}
 	}
 
+	protected function processDivMenu(&$result, $group, $menu)
+	{
+		foreach ($menu->children() as $row) {
+			if ($row->tag != 'div') {
+				continue;
+			}
+
+			$columns = array();
+			foreach ($row->children() as $column) {
+				if ($column->tag == 'div') {
+					$columns[] = $this->normalizeText($column->plaintext);
+				}
+			}
+
+			if (count($columns) < 2) {
+				continue;
+			}
+
+			$label = count($columns) > 2? array_shift($columns) : '';
+			$price = array_pop($columns);
+			$what = implode(' ', $columns);
+			if ($what == '' || $price == '') {
+				continue;
+			}
+
+			if (preg_match('/^Menu\s+\d+$/ui', $label)) {
+				$what = "$label: $what";
+			}
+
+			$result->dishes[] = new Dish($what, $price, NULL, $group);
+		}
+	}
+
 	protected function normalizeText($text)
 	{
-		return trim(html_entity_decode($text, ENT_QUOTES | ENT_HTML5, 'UTF-8'));
+		$text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+		$text = preg_replace('/\s+/u', ' ', $text);
+		return trim($text);
 	}
 }
